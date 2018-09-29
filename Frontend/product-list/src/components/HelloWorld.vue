af647606ad64126d408eec48db7ee5a110b8481a
Kamil <kludwiczuk@gmail.com>
2018-09-29 11:44:51 +0200

    Frontend init

---
+++ Frontend/product-list/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul>
+    <li>Product 1</li>
+    <li>Product 2</li>
+    <li>Product 3</li>
+    <li>Product 4</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
